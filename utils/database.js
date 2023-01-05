5511ad09995943dd8a4cca94c323a828a4c85db4
Saddam-tech <salokhiddinov0727@gmail.com>
2023-01-06 04:36:20 +0900

    app config

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,20 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+require("dotenv").config();
+const username = process.env.username;
+const pw = process.env.password;
 
-MongoClient.connect(
-  "mongodb+srv://saddamtech:19980727s@cluster0.t1dlpkc.mongodb.net/?retryWrites=true&w=majority"
-)
-  .then((res) => {
-    console.log("Connected!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+function mongoConnect(callback) {
+  MongoClient.connect(
+    `mongodb+srv://${username}:${pw}@cluster0.t1dlpkc.mongodb.net/?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      callback(client);
+      console.log("Connected!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+module.exports = mongoConnect;
